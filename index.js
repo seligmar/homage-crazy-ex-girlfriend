e0a187b201a41796a59e5b1be288757fac6800c2
github username <github email address>
2020-03-29 09:44:57 +0100

    added modal image

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const coin = document.querySelector('#coin')
+const modal_button = document.querySelector('#modal_button')
+const span = document.getElementsByClassName('close')[0]
+const modalImg = document.getElementById('img01')
+const captionText = document.querySelector('#caption')
+const img = document.getElementById('myImg')
+var modal = document.getElementById('myModal')
+
 coin.addEventListener('click', e => flip(e))
+modal_button.addEventListener('click', e => modalImage(e))
+span.addEventListener('click', e => closeSpan(e))
 
 const flip = e => {
   e.preventDefault()
@@ -16,4 +25,17 @@ const flip = e => {
     src="https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1539290857/181012-fallon-crazy-ex-girlfriend-rachel-bloom-hero_sj988h"
     />`
   }
+}
+
+const modalImage = e => {
+  e.preventDefault()
+  modal.style.display = 'block'
+  modalImg.src =
+    'https://i.pinimg.com/originals/38/2c/55/382c55d97cdef0b260d7412a401df1a7.gif'
+  captionText.innerText = 'Im a bit griffindore'
+}
+
+const closeSpan = e => {
+  e.preventDefault()
+  modal.style.display = 'none'
 }
